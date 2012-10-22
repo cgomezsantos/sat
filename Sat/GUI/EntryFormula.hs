a7df5a08b94c8c31cff653cd0a145dd284a89964
alexgadea <alex.aegf@gmail.com>
2012-10-22 18:53:06 -0300

    Comenzando la implementación del treeView para ingresar fórmulas.

--- Sat/GUI/EntryFormula.hs
+++ Sat/GUI/EntryFormula.hs
@@ -1,23 +1,21 @@
 module Sat.GUI.EntryFormula where
 
 import Graphics.UI.Gtk hiding (eventButton, eventSent,get)
+import Graphics.UI.Gtk.Gdk.Events
 
 import Data.Tree
 
 import Control.Monad.Trans.RWS
 
+import qualified Data.Foldable as F
+
 import Sat.GUI.GState
 
-data DeclState = DNoState
-               | DUnknown
-               | DChecked
-               | DError
-
-data FormulaItem = FormulaItem { fiName  :: Entry }
+data FormulaItem = FormulaItem { fiName  :: String }
 
 initialFormulaForest :: GuiMonad (Forest FormulaItem)
 initialFormulaForest = io $ do
-            entry <- entryNew
+            entry <- return "Ingresar Fórmula."
             return $ [Node (FormulaItem entry) []]
 
 configEntryFormula :: TreeView -> GuiMonad ()
@@ -29,22 +27,33 @@ configEntryFormula tv = initialFormulaForest >>=
         setupEFList list = do
             content <- ask
             s <- get
---             io $               
---                 treeViewGetColumn tv 0 >>=                
---                 F.mapM_ (treeViewRemoveColumn tv) >>
---                 
---                 treeViewColumnNew >>= \colName ->
---                 treeViewSetHeadersVisible tv False >>
---                 treeViewSetModel tv list >>
---                 
---                 cellRendererTextNew >>= \renderer ->
---                 cellLayoutPackStart colName renderer False >>
---                 
---                 treeViewColumnSetSizing colName TreeViewColumnAutosize >>
---                 cellLayoutSetAttributes colName renderer list 
---                                     (\ind -> [ cellText := ind ^. declName ]) >>
---                                     
---                 treeViewAppendColumn tv colName >>
---                 treeViewGetSelection tv >>= \tree ->
---                 return ()
+            io $               
+                treeViewGetColumn tv 0 >>=
+                F.mapM_ (treeViewRemoveColumn tv) >>
+                
+                treeViewGetSelection tv >>= \seltv ->
+                treeSelectionSetMode seltv SelectionSingle >>
+                
+                treeViewColumnNew >>= \colName ->
+                treeViewSetHeadersVisible tv False >>
+                treeViewSetModel tv list >>
+                
+                cellRendererTextNew >>= \renderer ->
+                set renderer [ cellTextEditable := True
+                             ] >>
+                -- Acá ghci sugiere usar on obj edited, en lugar de onEdited.
+                -- El 1 en treeStoreInsert debería ser el tamaño de la list.
+                on renderer edited (\tp s -> treeStoreChange list tp (const $ FormulaItem s) >> 
+                                             treeStoreInsert list [] 1 (FormulaItem $ "Ingresar Fórmula.") >>
+                                             return ()) >>
+                
+                cellLayoutPackStart colName renderer False >>
+                
+                treeViewColumnSetSizing colName TreeViewColumnAutosize >>
+                
+                cellLayoutSetAttributes colName renderer list 
+                                    (\ind -> [ cellText := fiName ind ]) >>
+                                    
+                treeViewAppendColumn tv colName >>
+                return ()
             return ()
